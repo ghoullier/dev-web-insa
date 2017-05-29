c16d70d031cf19e396793f057b0d02da790a82db
Grégory Houllier <gregory.houllier@gmail.com>
2017-05-29 14:12:49 +0200

    chore(structure) Finalize etape 05

--- webapp/src/app/weather-api.service.ts
+++ webapp/src/app/weather-api.service.ts
@@ -5,8 +5,8 @@ export class WeatherApiService {
 
   constructor() { }
 
-  async getWeather() {
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=Rennes,fr&appid=e7a7e42f9288170a490c5fca2d91f5df`;
+  async getWeather(city = 'Rennes,fr') {
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=e7a7e42f9288170a490c5fca2d91f5df`;
     return fetch(url).then((response) => response.json())
   }
 
--- webapp/src/app/weather/weather.component.ts
+++ webapp/src/app/weather/weather.component.ts
@@ -1,6 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 import { WeatherApiService } from './../weather-api.service';
+
+interface WeatherCity {
+  city: string
+}
 
 @Component({
   selector: 'app-weather',
@@ -8,10 +12,14 @@ import { WeatherApiService } from './../weather-api.service';
   styleUrls: ['./weather.component.css'],
   providers: [WeatherApiService]
 })
-export class WeatherComponent implements OnInit {
+export class WeatherComponent implements OnChanges, OnInit, WeatherCity {
+  @Input() city: string;
   weather;
   constructor(private api: WeatherApiService) {}
   async ngOnInit() {
-    this.weather = await this.api.getWeather()
+    this.weather = await this.api.getWeather(this.city)
+  }
+  async ngOnChanges(changes: SimpleChanges) {
+    this.weather = await this.api.getWeather(changes.city.currentValue);
   }
 }
